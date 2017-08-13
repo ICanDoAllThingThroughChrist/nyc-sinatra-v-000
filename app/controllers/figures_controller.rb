58be22c947bf8fc31ed6e7f03edc81286cdff75d
charlie lee <ctlee9096@gmail.com>
2017-08-13 04:48:35 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,13 +1,36 @@
-class FiguresController < Sinatra::Base
+class FiguresController < ApplicationController
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
   get '/figures/new' do
-    erb :new
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/new'
   end
 
   post '/figures' do
-    binding.pry
-    @figure = Figure.create(:name => params[:figure], :landmarks_id => params[:landmark], :title_id => params[:title])
+    #binding.pry
+    @figure = Figure.create(params["figure"])
+    redirect "figures/#{@figure.id}"
   end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    #binding.pry
+    @figure.landmark = params[:landmark]
+    @figure.title = params[:title]
+  end
+
+  delete '/figures/:id/delete' do #delete action
+    @figure = Figure.find_by_id(params[:id])
+    @figure.delete
+  redirect to '/figures'
 end
-# form to create a new figure
-#https://learn.co/tracks/full-stack-web-dev-with-react/
-#sinatra/activerecord/sinatra-restful-routes
+end
