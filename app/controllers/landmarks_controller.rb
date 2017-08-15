7422342828c7cd060af620115e47f2a2686dee63
charlie lee <ctlee9096@gmail.com>
2017-08-15 20:28:41 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -27,7 +27,6 @@ class LandmarksController < ApplicationController
   end
 
   patch '/landmarks/:id' do
-    @landmark = Landmark.find_by_id(params[:id])
-    
+    @landmark = Landmark.find_by_id(params[:id]) 
   end
 end
