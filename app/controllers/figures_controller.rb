2d078ef347a15cfa66671709ca323609eb8e2434
charlie lee <ctlee9096@gmail.com>
2017-08-15 16:27:52 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,4 +1,5 @@
 class FiguresController < ApplicationController
+
   get '/figures' do
     @figures = Figure.all
     erb :'/figures/index'
@@ -13,10 +14,25 @@ class FiguresController < ApplicationController
    get '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
     erb :'figures/show'
-  end
+   end
 
   post '/figures' do
      @figure = Figure.create(name: params["figure"]["name"])
+     #binding.pry
+    if params["figure"]["title_ids"] !=nil
+       params["figure"]["title_ids"].each do |title_id|@figure.titles << Title.find_by_id(title_id) end
+    end
+    if Title.find_by_name(params["title"]["name"])
+      @figure.titles << Title.create(name: params["title"]["name"])
+    end
+    if params["figure"]["landmark_ids"] != nil
+      params["figure"]["landmark_ids"].each do
+      |x| @figure.landmarks << Landmark.find_by_id(x)
+      end
+    end
+    if !Landmark.find_by_name(params["landmark"]["name"]) && params["figure"]["landmark_ids"] != nil
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"])
+    end
      @figure.save
      redirect to "/figures/#{@figure.id}"
   end
@@ -50,7 +66,7 @@ class FiguresController < ApplicationController
   delete '/figures/:id/delete' do #delete action
     @figure = Figure.find_by_id(params[:id])
     @figure.delete
-  redirect to '/figures'
-end
+    redirect to '/figures'
+   end
 
 end
